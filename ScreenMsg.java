11b297448729625a5585fc83e4448f9f811761b5
1120023921 <1120023921@qq.com>
2020-05-28 20:14:31 +0800

    Create ScreenMsg.java

---
+++ ScreenMsg.java
@@ -0,0 +1,23 @@
+package com.cintsoft.ace.sbms.provider.api.vo.screen;
+
+import java.util.Map;
+
+/**
+ * @author 胡昊
+ * Description: 大屏交互信息
+ * Date: 2020/2/20
+ * Time: 13:57
+ * Mail: huhao9277@gmail.com
+ */
+public class ScreenMsg {
+
+    private String appid;
+    private String secret;
+    private String deviceId;
+    private String action;
+    private Map<String, Object> msg;
+
+    public static void main(String[] args) {
+        System.out.println("hello world---");
+    }
+}
